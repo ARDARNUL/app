949ab35c146d9714e83546337d8750e6419f8d79
ardarnul <obozin199@mail.ru>
2024-01-15 17:04:51 +0700

    Task 5(bonus) and 6

--- js/main.js
+++ js/main.js
@@ -11,14 +11,41 @@ let app = new Vue({
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
         {
-        variantId: 2234,
-        variantColor: 'green'
+            variantId: 2234,
+            variantColor: 'green'
         },
         {
-        variantId: 2235,
-        variantColor: 'blue'
-        }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+            variantId: 2235,
+            variantColor: 'blue'
+        }],
+        cart: 0,
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        variants: [
+            {
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage: "../img/vmSocks-green-onWhite.jpg",
+            },
+            {
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: "../img/vmSocks-blue-onWhite.jpg",
+            }
+            ],
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeToCart() {
+            this.cart -= 1
+            if (this.cart < 0){
+                this.cart = 0
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+            }
     }
 })
+
