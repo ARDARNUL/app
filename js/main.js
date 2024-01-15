512b89b94bc98662659a1ae29f22bf2dab76cbe8
ardarnul <obozin1998@mail.ru>
2024-01-15 09:44:36 +0700

    2 and 3 task (arr + if else)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+    product: "Socks",
+    image: "../img/vmSocks-green-onWhite.jpg",
+    altText: "A pair of socks",
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+        {
+        variantId: 2234,
+        variantColor: 'green'
+        },
+        {
+        variantId: 2235,
+        variantColor: 'blue'
+        }
+        ]
+    }
+})
+
